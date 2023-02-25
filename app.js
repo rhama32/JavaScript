6060f9a616f69db972f3da1e083f226cd805b469
rhama32 <r.hamaoka32915@gmail.com>
2023-02-25 09:15:50 +0000

    Java

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
-alert('今日は');
+let hello = "Hello World";
+alert(hello);
+
+let int1 = 1;
+alert(int1)
+
+let int2 = -10;
+alert(int2)
+
+let int3 = "4+2"
+alert(int3)
+
+let int4 = "Javascript"
+
+alert(int4)
+
+alert(4 + 3)
+
+alert(4 * 3)
+
+alert(4-3)
+
+alert(10 / 2);
+
+alert('Hello' + 'World');
+
+let str1 = 'Hello';
+let str2 = 'World!!';
+alert(str1 + str2);
+
+if(条件1){
+  処理1
+}
+// 条件1を満たすときは、処理1を実行
+
+let orange = 100;
+let apple = 120;
+
+if(orange < apple){
+  alert('みかんの値段がりんごより安い');
+} else if(orange == apple){
+  alert('みかんとりんごが同じ値段');
+} else{
+  alert('みかんの値段がりんごより高い');
+}
