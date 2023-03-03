4a507aaba83390c9e252975f6cd810eb266f7fae
rhama32 <r.hamaoka32915@gmail.com>
2023-03-03 07:40:39 +0000

    Javascript

---
+++ main.js
@@ -0,0 +1,74 @@
+//strictモードで実行。厳密にエラーを検証。
+'use strict';
+
+
+
+// //変数　= 値を変更できる
+// let color = 'Red';
+// console.log(color);
+
+// color = 'Green';
+// console.log(color);
+
+// //let color = 'Blue';
+
+// let myColor = 'Blue';
+// // let = ES2015より導入
+// console.log(myColor);
+
+// var myName = 'Mike';
+// console.log(myName);
+
+// // 定数 = 値を変更出来ない
+// const PI = 3.14;
+// console.log(PI);
+// // PI = 3.1; NG
+
+// const TAX_RATE = 10;
+// // _で繋ぐことスネークケース
+// console.log(TAX_RATE);
+
+
+// console.log('Hello! I\'m Mike.Array');
+// console.log('\¥\t AAA \n BBB');
+// console.log("Hello I'm Mike Array");
+// console.log('こんにちは"マイクさん"');
+// console.log('Hello'); // 同一プロジェクトでは統一
+// console.log("Hello"); // 同一プロジェクトでは統一
+
+// console.log(12345); //数値リテラル
+// console.log(12345.6789); //浮動小数点リテラル
+// console.log('12345')　//文字列リテラル
+
+// console.log('Hello World!!!')
+// console.log('Hello World!!!')
+// console.log('Hello World!!!')
+
+
+//<Javascriptとは？>
+//実行環境がWebブラウザに実装
+//Webページでの動的な機能を実現
+//ウェブブラウザで動作する唯一の言語
+
+//<利用例>
+//Webページの再読み込みなしで読み込みが可能。
+//Webページ要素のアニメーション・フェードイン・アウト
+//フォームの入力値の動的なチェック
+
+//Javascriptのバージョン（ECMAScriptのバージョン）
+//基本はES2015をベースでWebブラウザの対応状況次第で最新を使用。
+//上位互換性がある。
+
+//調査＝MDN・ECMAScript
+
+//コメントアウト時に//を付けてもブラウザでは見れるため覚えておく。
+
+// console.log('Hello');
+
+// リテラルとは？
+// プログラム内に直接記述した値。
+
+//　エスケープシーケンス
+//特殊な文字や機能を実現するもの
+
+//予約語はMDN確認
