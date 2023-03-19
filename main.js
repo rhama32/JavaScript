baf50c49cef559462b015ac595492382e2a9637f
rhama32 <r.hamaoka32915@gmail.com>
2023-03-19 13:00:04 +0000

    三項演算子

--- main.js
+++ main.js
@@ -1,42 +1,46 @@
 //strictモードで実行。厳密にエラーを検証。
 'use strict';
 
-let num = 6;
-if(num === 1) {
-  console.log('大吉');
-} else if(num === 2 ) {
-  console.log('中吉');
-} else if(num === 3 ) {
-  console.log('小吉');
-} else if(num === 4 ) {
-  console.log('吉');
-} else if(num === 5 ) {
-  console.log('凶');
-} else {
-  console.log('想定外');
-}
+let age = 25;
+let beverage = (age >= 20)? 'ビール' : 'コーラ';
+console.log(beverage);
 
-let num = Math.floor(Math.random() * 5 + 1);
-switch (num) {
-  case 1:
-    console.log('大吉');
-    break;
-  case 2:
-    console.log('中吉');
-    break;
-  case 3:
-    console.log('小吉');
-    break;
-  case 4:
-    console.log('吉');
-    break;
-  case 5:
-    console.log('凶');
-    break;
-  default :
-    console.log('想定外');
-    break;
-}
+// let num = 6;
+// if(num === 1) {
+//   console.log('大吉');
+// } else if(num === 2 ) {
+//   console.log('中吉');
+// } else if(num === 3 ) {
+//   console.log('小吉');
+// } else if(num === 4 ) {
+//   console.log('吉');
+// } else if(num === 5 ) {
+//   console.log('凶');
+// } else {
+//   console.log('想定外');
+// }
+
+// let num = Math.floor(Math.random() * 5 + 1);
+// switch (num) {
+//   case 1:
+//     console.log('大吉');
+//     break;
+//   case 2:
+//     console.log('中吉');
+//     break;
+//   case 3:
+//     console.log('小吉');
+//     break;
+//   case 4:
+//     console.log('吉');
+//     break;
+//   case 5:
+//     console.log('凶');
+//     break;
+//   default :
+//     console.log('想定外');
+//     break;
+// }
 
 
 // let num = 3;
