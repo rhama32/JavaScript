930d6fa4499bfba7db53ee39eb2036e0db7d6dd6
rhama32 <r.hamaoka32915@gmail.com>
2023-03-22 03:50:05 +0000

    配列

--- main.js
+++ main.js
@@ -1,16 +1,31 @@
 //strictモードで実行。厳密にエラーを検証。
 'use strict';
 
-function greeting(name) {
-  console.log('Hello' + name + 'さん');
-}
+let colors = ['Red','Green','Blue'];
+console.log(colors);
+console.log(colors[0]);
+console.log(colors[1]);
+console.log(colors[2]);
+console.log(colors[3]);
 
-function inputUserName(callback) {
-  let name = prompt('あなたの名前を入力してください');
-  callback(name);
-}
+let arr = [1,'2',3,['a','b','c']];
+console.log(arr)
+console.log(colors.length);
+console.log(colors[colors.length -1]);
 
-inputUserName(greeting);
+let emptyArr = [];
+console.log(emptyArr);
+
+// function greeting(name) {
+//   console.log('Hello' + name + 'さん');
+// }
+
+// function inputUserName(callback) {
+//   let name = prompt('あなたの名前を入力してください');
+//   callback(name);
+// }
+
+// inputUserName(greeting);
 
 
 // const displayMessage = function() {
