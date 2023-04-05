4064c9f7caafa972959378c025e92d85cead7158
rhama32 <r.hamaoka32915@gmail.com>
2023-04-05 11:11:36 +0000

    ループ

--- main.js
+++ main.js
@@ -1,16 +1,43 @@
 //strictモードで実行。厳密にエラーを検証。
 'use strict';
 
-for (let i= 1; i <= 3; i++) {
-  console.log(i);
-}
+// for (let i= 1; i <= 3; i++) {
+//   console.log(i);
+// }
 
 
-let arr = ['a','b','c'];
-for(let i = 0; i < 3; i++) {
-  console.log(arr[i]);
+// let arr = ['a','b','c'];
+// for(let i = 0; i < 3; i++) {
+//   console.log(arr[i]);
+// }
+
+// for(let i = 0; i < 3; i++) {
+//   for(let j = 0; j < 3; j++) {
+//     console.log(`i: ${i}, j: ${j}`);
+//   }
+// }
+
+const scores = [
+  [100, 99, 98],
+  [90,89,88],
+  [80,79,78],
+  ];
+  
+// console.log(scores[0][0]);
+// console.log(scores[0][1]);
+// console.log(scores[0][2]);
+// console.log(scores[1][0]);
+// console.log(scores[1][1]);
+// console.log(scores[1][2]);
+// console.log(scores[2][0]);
+// console.log(scores[2][1]);
+// console.log(scores[2][2]);
+
+for(let i = 0; i < scores.length; i++) {
+  for(let j = 0; j < scores[i].length; j++) {
+   console.log(scores[i][j]);
+  }
 }
- 
 
 // let birthyear = '2000';
 // let age = '18';
