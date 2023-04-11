adb88ab23822675f8dfa54787e63489ce98e7d7e
rhama32 <r.hamaoka32915@gmail.com>
2023-04-11 12:56:38 +0000

    Windowオブジェクト

--- main.js
+++ main.js
@@ -1,34 +1,50 @@
 //strictモードで実行。厳密にエラーを検証。
 'use strict';
 
-let addButton = document.getElementById('addButton');
-addButton.addEventListener('click', function(e) {
-  //入力欄のinput要素を取得
-  let inputLanguage = document.getElementById('inputLanguage');
-  //input要素から文字列を取得
-  let language = inputLanguage.value;
-  if(language === ''){
-    alert('未入力です');
-    return;
-  }
-  //li要素を生成
-  let listItem = document.createElement('li');
-  //li要素のコンテンツに入力された文字列を設定
-  listItem.textContent = language;
-  //ul要素を取得
-  let languageList = document.getElementById('languageList');
-  //listの末尾に生成した要素を追加
-  languageList.appendChild(listItem);
-  //入力欄をクリア
-  inputLanguage.value =''; 
-}, false);
+// window.console.log('hello');
 
-//削除
-let removeButton = document.getElementById('removeButton');
-removeButton.addEventListener('click', function(e) {
-  let languageList = document.getElementById('languageList');
-  languageList.removeChild(languageList.lastElementChild);
-}, false);
+// console.log('hello');
+
+// window.alert('Are you OK?');
+
+// window.confirm('Are you OK?');
+
+//window.open('https://google.co.jp');
+
+//スクロール
+let btn = document.getElementById('triggerButton');
+btn.addEventListener('click', function(e) {
+  window.scroll(0,300);
+},false);
+
+// let addButton = document.getElementById('addButton');
+// addButton.addEventListener('click', function(e) {
+//   //入力欄のinput要素を取得
+//   let inputLanguage = document.getElementById('inputLanguage');
+//   //input要素から文字列を取得
+//   let language = inputLanguage.value;
+//   if(language === ''){
+//     alert('未入力です');
+//     return;
+//   }
+//   //li要素を生成
+//   let listItem = document.createElement('li');
+//   //li要素のコンテンツに入力された文字列を設定
+//   listItem.textContent = language;
+//   //ul要素を取得
+//   let languageList = document.getElementById('languageList');
+//   //listの末尾に生成した要素を追加
+//   languageList.appendChild(listItem);
+//   //入力欄をクリア
+//   inputLanguage.value =''; 
+// }, false);
+
+// //削除
+// let removeButton = document.getElementById('removeButton');
+// removeButton.addEventListener('click', function(e) {
+//   let languageList = document.getElementById('languageList');
+//   languageList.removeChild(languageList.lastElementChild);
+// }, false);
 
 
 // //class属性を追加更新(赤)
