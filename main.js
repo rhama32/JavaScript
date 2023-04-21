45b01eadc77df11ed23c335ac392f8b8028b69eb
rhama32 <r.hamaoka32915@gmail.com>
2023-04-21 13:18:54 +0000

    おみくじ

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
 //strictモードで実行。厳密にエラーを検証。
 'use strict';
 
-// let num = Math.floor(Math.random() * 5 + 1);
-// switch (num) {
-//   case 1:
-//     console.log('大吉');
-//     break;
-//   case 2:
-//     console.log('中吉');
-//     break;
-//   case 3:
-//     console.log('小吉');
-//     break;
-//   case 4:
-//     console.log('吉');
-//     break;
-//   case 5:
-//     console.log('凶');
-//     break;
-//   default :
-//     console.log('想定外');
-//     break;
-// }
+let num = Math.floor(Math.random() * 5 + 1);
+switch (num) {
+  case 1:
+    console.log('大吉');
+    break;
+  case 2:
+    console.log('中吉');
+    break;
+  case 3:
+    console.log('小吉');
+    break;
+  case 4:
+    console.log('吉');
+    break;
+  case 5:
+    console.log('凶');
+    break;
+  default :
+    console.log('想定外');
+    break;
+}
 
 // console.log('Hello! I\'m Mike.Array');
 // console.log('\¥\t AAA \n BBB');
