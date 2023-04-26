1f43a8a2d4dfb2e64c4dac3120545ca0ce9a78d8
rhama32 <r.hamaoka32915@gmail.com>
2023-04-26 13:43:01 +0000

    プログラム

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
 //strictモードで実行。厳密にエラーを検証。
 'use strict';
 
-let num = Math.floor(Math.random() * 5 + 1);
-switch (num) {
-  case 1:
-    console.log('大吉');
-    break;
-  case 2:
-    console.log('中吉');
-    break;
-  case 3:
-    console.log('小吉');
-    break;
-  case 4:
-    console.log('吉');
-    break;
-  case 5:
-    console.log('凶');
-    break;
-  default :
-    console.log('想定外');
-    break;
-}
+// let num = Math.floor(Math.random() * 5 + 1);
+// switch (num) {
+//   case 1:
+//     console.log('大吉');
+//     break;
+//   case 2:
+//     console.log('中吉');
+//     break;
+//   case 3:
+//     console.log('小吉');
+//     break;
+//   case 4:
+//     console.log('吉');
+//     break;
+//   case 5:
+//     console.log('凶');
+//     break;
+//   default :
+//     console.log('想定外');
+//     break;
+// }
 
   // //説明の初期値を設定
   // let description = document.createElement('p');
